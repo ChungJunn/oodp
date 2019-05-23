667c84266a00477bcf8ff8d43748deaa08bc14e8
JangJuMan <21700633@handong.edu>
2019-05-23 21:40:07 +0900

    juman
    drawing pad1

--- DrawSystem.java
+++ DrawSystem.java
@@ -23,90 +23,139 @@ public class DrawSystem extends JFrame {
 			}
 		});
 	}
-//	public DrawSystem() {
-//		setTitle("Drawing Pad");
-//		canvas = new ScribbleCanvas();
-//		getContentPane().setLayout(new BorderLayout());
-//		getContentPane().add(CreateMenuBar(), BorderLayout.NORTH);
-//		getContentPane().add(canvas,  BorderLayout.CENTER);
-//		
-////		canvas.setBackground(Color.white);
-////		getContentPane().setLayout(new BorderLayout());
-////		getContentPane().add(canvas,  BorderLayout.CENTER);
-//		
-//		addWindowListener(new WindowAdapter() {
-//			public void windowClosing(WindowEvent e) {
-//				if(exitAction != null) {
-//					exitAction.actionPerformed(new ActionEvent(DrawSystem.this, 0, null));
-//				}
-//			}
-//		});
-//		
-////		<createMenuBar() method on page 411>
-////		<newFile(), openFile(), saveFile(), and saveFileAs() method on page 416>
-//		
-//	} 
+	
 	protected JMenuBar CreateMenuBar() {
+		
+		//after refactoring
 		JMenuBar menuBar = new JMenuBar();
-		JMenu menu;
-		JMenuItem mi;
+		JMenuItem [] mi = new JMenuItem[5];
+		String[] FileMenu = {"New", "Open", "Save", "Save as", "Exit"};
+		JMenu menu = new JMenu("File");
 		
-		// File menu
-		menu = new JMenu("File");
+		MenuActionListener listener = new MenuActionListener();
+		// file menu
+		for(int i=0; i<mi.length; i++) {
+			if(i == 4) {
+				menu.add(new JSeparator());
+			}
+			mi[i] = new JMenuItem(FileMenu[i]);
+			mi[i].addActionListener(listener);
+			menu.add(mi[i]);
+		}
 		menuBar.add(menu);
-		
-		mi = new JMenuItem("New");
-		menu.add(mi);
-		mi.addActionListener(new NewFileListener());
-		
-		mi = new JMenuItem("Open");
-		menu.add(mi);
-		mi.addActionListener(new OpenFileListener());
-		
-		mi = new JMenuItem("Save");
-		menu.add(mi);
-		mi.addActionListener(new SaveFileListener());
-		
-		mi = new JMenuItem("Save As");
-		menu.add(mi);
-		mi.addActionListener(new SaveAsFileListener());
-		
-		menu.add(new JSeparator());
-		
-		exitAction = new ExitListener();
-		mi = new JMenuItem("Exit");
-		menu.add(mi);
-		mi.addActionListener(exitAction);
 		
 		// option menu
+		JMenuItem [] mi2 = new JMenuItem[1];
 		menu = new JMenu("Option");
+		String[] OptionMenu = {"Color"};
+		for(int i=0; i<mi2.length; i++) {
+			if(i == 4) {
+				menu.add(new JSeparator());
+			}
+			mi2[i] = new JMenuItem(OptionMenu[i]);
+			mi2[i].addActionListener(listener);
+			menu.add(mi2[i]);
+		}
 		menuBar.add(menu);
 		
-		mi = new JMenuItem("Color");
-		menu.add(mi);
-		mi.addActionListener(new ColorListener());
-		
-		// new - tools
-		menu = new JMenu("Tool");
+		// tool menu
+		JMenuItem [] mi3 = new JMenuItem[1];
+		menu = new JMenu("tool");
+		String[] ToolMenu = {"Clear"};
+		for(int i=0; i<mi3.length; i++) {
+			if(i == 4) {
+				menu.add(new JSeparator());
+			}
+			mi3[i] = new JMenuItem(ToolMenu[i]);
+			mi3[i].addActionListener(listener);
+			menu.add(mi3[i]);
+		}
 		menuBar.add(menu);
-			
-		mi = new JMenuItem("Clear");
-		menu.add(mi);
-		mi.addActionListener(new ClearListener());
-				
+
 		//horizontal space
 		menuBar.add(Box.createHorizontalGlue());
 		
-		//Help menu
-		menu = new JMenu("Help"); 
-		menuBar.add(menu);
+		// help menu
+		JMenuItem [] mi4 = new JMenuItem[1];
+		menu = new JMenu("Help");
+		String[] HelpMenu = {"About"};
+		for(int i=0; i<mi4.length; i++) {
+			if(i == 4) {
+				menu.add(new JSeparator());
+			}
+			mi4[i] = new JMenuItem(HelpMenu[i]);
+			mi4[i].addActionListener(listener);
+			menu.add(mi4[i]);
+		}
 		
-		mi = new JMenuItem("About");
-		menu.add(mi);
-		mi.addActionListener(new AboutListener());
+		menuBar.add(menu);
 		
 		return menuBar;
 		
+	}
+	
+	//after refactoring
+	class MenuActionListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			String cmd = e.getActionCommand();
+			switch(cmd) {
+				case "New":
+					newFile();
+					break;
+					
+				case "Open":
+					int retval = chooser.showDialog(null, "Open");
+					if(retval == JFileChooser.APPROVE_OPTION) {
+						File theFile = chooser.getSelectedFile();
+						if(theFile != null) {
+							String filename = chooser.getSelectedFile().getAbsolutePath();
+							openFile(filename);
+						}
+					}
+					break;
+					
+				case "Save":
+					saveFile();
+					break;
+					
+				case "Save as":
+					int retval2 = chooser.showDialog(null, "Save As");
+					if(retval2 == JFileChooser.APPROVE_OPTION) {
+						File theFile = chooser.getSelectedFile();
+						if(theFile != null) {
+							if(!theFile.isDirectory()) {
+								String filename = chooser.getSelectedFile().getAbsolutePath();
+								saveFileAs(filename);
+							}
+						}
+					}
+					break;
+					
+				case "Exit":
+					int result = JOptionPane.showConfirmDialog(null, "Do you want to exit Drawing pad?", "Exit Drawing Pad?", JOptionPane.YES_NO_OPTION);
+					if(result == JOptionPane.YES_NO_OPTION) {
+						saveFile();
+						System.exit(0);
+					}
+					break;
+					
+				case "Color":
+					ColorDialog dialog_color = new ColorDialog(DrawSystem.this, "Choose color", canvas.getCurColor());
+					Color result_color = dialog_color.showDialog();
+					if(result_color != null) {
+						canvas.setCurColor(result_color);
+					}
+					break;
+					
+				case "Clear":
+					clear();
+					break;
+					
+				case "About":
+					JOptionPane.showMessageDialog(null, "DrawingPad version 1.0\nCopyright (c) Xiaoping Jia 2002", "About", JOptionPane.INFORMATION_MESSAGE);
+					break;
+			}
+		}
 	}
 	
 	// factory method
@@ -130,71 +179,6 @@ public class DrawSystem extends JFrame {
 	protected ActionListener exitAction;
 	protected JFileChooser chooser = new JFileChooser(".");
 	
-	
-	
-	class AboutListener implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
-			JOptionPane.showMessageDialog(null, "DrawingPad version 1.0\nCopyright (c) Xiaoping Jia 2002", "About", JOptionPane.INFORMATION_MESSAGE);
-		}
-	}
-	
-	class NewFileListener implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
-			newFile();
-		}
-	}
-	
-	class OpenFileListener implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
-			int retval = chooser.showDialog(null, "Open");
-			if(retval == JFileChooser.APPROVE_OPTION) {
-				File theFile = chooser.getSelectedFile();
-				if(theFile != null) {
-					String filename = chooser.getSelectedFile().getAbsolutePath();
-					openFile(filename);
-				}
-			}
-		}
-	}
-	
-	class SaveFileListener implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
-			saveFile();
-		}
-	}
-	
-	class SaveAsFileListener implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
-			int retval = chooser.showDialog(null, "Save As");
-			if(retval == JFileChooser.APPROVE_OPTION) {
-				File theFile = chooser.getSelectedFile();
-				if(theFile != null) {
-					if(!theFile.isDirectory()) {
-						String filename = chooser.getSelectedFile().getAbsolutePath();
-						saveFileAs(filename);
-					}
-				}
-			}
-		}
-	}
-	
-	class ClearListener implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
-			clear();
-		}
-	}
-	
-	
-	class ExitListener implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
-			int result = JOptionPane.showConfirmDialog(null, "Do you want to exit Drawing pad?", "Exit Drawing Pad?", JOptionPane.YES_NO_OPTION);
-			if(result == JOptionPane.YES_NO_OPTION) {
-				saveFile();
-				System.exit(0);
-			}
-			
-		}
-	}
 	
 	
 	protected void clear() {
@@ -227,16 +211,17 @@ public class DrawSystem extends JFrame {
 		setTitle("Drawing pad [ "+ currentFilename + "]");
 	}
 	
-	class ColorListener implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
-			Color result = dialog.showDialog();
-			if(result != null) {
-				canvas.setCurColor(result);
-			}
-		}
-		
-		protected ColorDialog dialog = new ColorDialog(DrawSystem.this, "Choose color", canvas.getCurColor());
-	}
+	// 만일을 위해 보존할 것. protected 부분이 삭제됨 위에서
+//	class ColorListener implements ActionListener{
+//		public void actionPerformed(ActionEvent e) {
+//			Color result = dialog.showDialog();
+//			if(result != null) {
+//				canvas.setCurColor(result);
+//			}
+//		}
+//		
+//		protected ColorDialog dialog = new ColorDialog(DrawSystem.this, "Choose color", canvas.getCurColor());
+//	}
 	
 	JMenuBar menuBar = new JMenuBar();
 
